a4c54c4f519f77d4f851424bd98ba4fbfb5d0f19
abdullahteymurov <abdullah.teymurov.2004@gmail.com>
2024-03-12 15:26:26 +0400

    Abu  has done all tasks

---
+++ abdullahteymurov/index.js
@@ -0,0 +1,37 @@
+// task 1
+
+// let price = prompt("qiymeti daxil edin(xx.x seklinde)");
+
+// let priceStr = String(price);
+
+// let priceArray = priceStr.split(".");
+
+// console.log(priceArray);
+
+// let manat = priceArray[0];
+
+// let qepik = priceArray[1];
+
+// console.log(manat + " manat " + qepik + " qepik");
+
+// task 2
+
+// let salary = prompt("emek haqqi:");
+// let vergisiz = salary * 0.87;
+// console.log(vergisiz);
+
+// task 3
+
+// let user = prompt('soyad, ad, ata adi:');
+
+// let tamArray = user.split(" ");
+// console.log(tamArray);
+
+// let soyad = tamArray[0];
+// let adBash = tamArray[1][0];
+// let ataadBash = tamArray[2][0];
+
+// console.log(soyad + " " + adBash + "." + ataadBash);
+
+// star task
+
